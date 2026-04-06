a63957c67a89b9e523b92ae5f4c15988335365d9
wb <fzadddfasdfsafa@gmail.com>
2026-04-07 03:43:38 +1100

    Update main.py
    Неиспользуемая библиотека

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import streamlit as st
 import plotly.graph_objects as go
 import plotly.io as pio
 import tempfile
-import os
 
 from pandas import DataFrame
 from plotly.subplots import make_subplots
